e7e191cd3e6e96ea14f6bfea3041e8d5fd675bd3
zalzdarkent <111816011+zalzdarkent@users.noreply.github.com>
2024-06-14 08:10:50 +0700

    Update edit.blade.php

--- Modules/People/Resources/views/customers/edit.blade.php
+++ Modules/People/Resources/views/customers/edit.blade.php
@@ -38,7 +38,7 @@
                                 <div class="col-lg-4">
                                     <div class="form-group">
                                         <label for="customer_phone">Nomor Pelanggan <span class="text-danger">*</span></label>
-                                        <input type="text" class="form-control" name="customer_phone" required value="{{ $customer->customer_phone }}">
+                                        <input type="number" class="form-control" name="customer_phone" required value="{{ $customer->customer_phone }}">
                                     </div>
                                 </div>
                                 <div class="col-lg-4">
